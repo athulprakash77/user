0dcce9138fb7dfcb7089d979e0e25121a8a52edf
athulprakash007 <athull362@gmail.com>
2022-11-15 21:05:51 -0800

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,31 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdduserComponent } from './adduser/adduser.component';
+import { ViewallComponent } from './viewall/viewall.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+
+const myroutes:Routes=[
+  {
+    path:"",component:AdduserComponent},
+    {path:"viewall",component:ViewallComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdduserComponent,
+    ViewallComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+RouterModule.forRoot(myroutes)
+ ],
   providers: [],
   bootstrap: [AppComponent]
 })
